c180b69a2e5de11b5ef6dafed695de5264e23c11
varya979 <varya979@yandex.ru>
2023-05-24 18:27:58 +0300

    feat: создать package.json, editorconfig, eslint, git-репозиторий, app.js, хот-релоуд

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
