1bf559cb3a5e7b25be466563eef46ea04d7e65b8
varya979 <varya979@yandex.ru>
2023-05-29 13:59:44 +0300

    fix: ошибки eslint

--- models/card.js
+++ models/card.js
@@ -1,42 +1,44 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  link: {
-    type: String,
-    required: [true, 'Поле "link" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректная ссылка',
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    link: {
+      type: String,
+      required: [true, 'Поле "link" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректная ссылка',
+      },
+    },
+    owner: {
+      /* Лучшая ссылка из одного документа на другой — идентификатор.
+      Mongo автоматически создаёт поле `_id` — уникальный идентификатор
+      для каждого документа. Этот идентификатор позволяет связать один документ с другим.
+      Чтобы сделать это на уровне схемы, полю следует установить специальный тип —
+      `mongoose.Schema.Types.ObjectId` и свойство `ref`. */
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    likes: [{ // список лайкнувших пост пользователей
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    }],
+    createdAt: { // дата создания
+      type: Date,
+      default: Date.now,
     },
   },
-  owner: {
-    /* Лучшая ссылка из одного документа на другой — идентификатор.
-    Mongo автоматически создаёт поле `_id` — уникальный идентификатор
-    для каждого документа. Этот идентификатор позволяет связать один документ с другим.
-    Чтобы сделать это на уровне схемы, полю следует установить специальный тип —
-    `mongoose.Schema.Types.ObjectId` и свойство `ref`. */
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  likes: [{ // список лайкнувших пост пользователей
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-    default: [],
-  }],
-  createdAt: { // дата создания
-    type: Date,
-    default: Date.now,
-  },
-},
-{ versionKey: false }, // чтобы убрать поле __v (версию документа) из объекта(наглядно - в постмане)
+  { versionKey: false }, /* чтобы убрать поле __v (версию документа)
+  из объекта(наглядно - в постмане) */
 );
 
 module.exports = mongoose.model('card', cardSchema);
--- models/user.js
+++ models/user.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "about" - 2'],
-    maxlength: [30, 'Максимальная длина поля "about" - 30'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: 'Некорректная ссылка',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      required: [true, 'Поле "about" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "about" - 2'],
+      maxlength: [30, 'Максимальная длина поля "about" - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректная ссылка',
+      },
     },
   },
-},
-{ versionKey: false }, // чтобы убрать поле __v (версию документа) из объекта(наглядно - в постмане)
+  { versionKey: false }, /* чтобы убрать поле __v (версию документа)
+  из объекта(наглядно - в постмане) */
 );
 
 module.exports = mongoose.model('user', userSchema);
