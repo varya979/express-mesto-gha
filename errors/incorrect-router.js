f74e4a941188b294af127d5601609e62fd89c9f0
varya979 <varya979@yandex.ru>
2023-06-04 20:06:45 +0300

    feat: исправить ошибку тестов

--- errors/incorrect-router.js
+++ errors/incorrect-router.js
@@ -1,5 +1,7 @@
-const incorrectRouter = (req, res) => {
-  res.statusCode(404).send({ message: 'Указанный путь не существует' });
+const NotFoundError = require('./not-found-error');
+
+const incorrectRouter = (req, res, next) => {
+  next(new NotFoundError('Указанный путь не существует'));
 };
 
 module.exports = incorrectRouter;
